7ff1f3a530ea06cdfefb0db923bcb7dbb7c08e09
Waleed Waheed <waleed.waheed@globalnorthstar.com>
2024-01-23 22:50:17 +0500

    Walee

--- src/pages/ClubSetup.js
+++ src/pages/ClubSetup.js
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
-import { styled, useTheme, alpha } from "@mui/material/styles";
-import { Box, Typography, Tabs, Tab, Grid, FormControl, Input, InputLabel, InputBase, TextField } from "@mui/material";
+import { Box, Checkbox, Divider, FormControlLabel, Grid, Switch, Table, TableHead, Typography, styled } from "@mui/material";
 import CustomDrawer from "../components/Navigation/CustomDrawer";
 import PageHeader from "../ui/PageHeader";
 import PageBody from "../ui/PageBody";
-import { Label } from "@mui/icons-material";
-import FormControlCustom from "../ui/FormControlCustom";
+import StyledInputBase from "../ui/InputBase";
+import FormLabel from "../ui/FormLabel";
+import { GridRow } from "../ui/GridRow";
+import { useState } from "react";
+import CustomPhoneInput from "../ui/PhoneInput";
+import FormSectionTitle from "../ui/FormSectionTitle";
+import CustomTable from "../ui/CustomTable";
 
 const ClubSetup = () => {
+  //Phone Inputs
+  const [phones, setPhones] = useState(["", ""]);
+  const [additionalPhone, setAdditionalPhone] = useState("");
+  const labels = ["Phone # 1", "Phone # 2"];
+
+  const handlePhoneChange = (value, index) => {
+    const updatedPhones = [...phones];
+    updatedPhones[index] = value;
+    setPhones(updatedPhones);
+  };
+
+  const handleAdditionalPhoneChange = (value) => {
+    setAdditionalPhone(value);
+  };
+
+  //Table Data
+  const headers = ["Name", "Role", "Phone #", "Email Address"];
+  const data = [
+    ["John Doe", "Admin", "123-456-7890", "john@example.com"],
+    ["Jane Smith", "User", "987-654-3210", "jane@example.com"],
+    // Add more rows as needed
+  ];
+
   const DrawerHeader = styled("div")(({ theme }) => ({
     display: "flex",
     alignItems: "center",
@@ -22,9 +48,71 @@ const ClubSetup = () => {
       <Box className="page_outer" sx={{ backgroundColor: "rgba(199, 219, 229, 0.25)", borderRadius: "32px", overflow: "hidden" }}>
         <PageHeader title="Club Setup" />
         <PageBody>
-          <Grid container spacing={2}>
-            <Grid item md={4}></Grid>
-          </Grid>
+          <GridRow container spacing={4} justifyContent="space-between">
+            <Grid item md={3}>
+              <FormLabel htmlFor="clubName" isRequired={true}>
+                Club Name
+              </FormLabel>
+              <StyledInputBase id="clubName" />
+            </Grid>
+            <Grid item>
+              <FormLabel htmlFor="arch" isRequired={true}>
+                Archive
+              </FormLabel>
+              <FormControlLabel control={<Switch defaultChecked />} label={<Typography sx={{ fontSize: "14px", fontWeight: 600 }}>Enabled</Typography>} />
+            </Grid>
+          </GridRow>
+          <GridRow container spacing={4}>
+            <Grid item md={3}>
+              <FormLabel htmlFor="clubType" isRequired={true}>
+                Type
+              </FormLabel>
+              <Box xs={{ gap: "25px" }}>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="KB" />
+                <FormControlLabel control={<Checkbox />} label="OTB" />
+                <FormControlLabel control={<Checkbox />} label="Other" />
+              </Box>
+            </Grid>
+            <Grid item md={3}>
+              <FormLabel htmlFor="clubType" isRequired={true}>
+                Staff
+              </FormLabel>
+              <Box xs={{ gap: "25px" }}>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Fully Volunteer Run Club" />
+              </Box>
+            </Grid>
+            <Grid item md={3}>
+              <FormLabel htmlFor="clubType" isRequired={true}>
+                Source of Entrants
+              </FormLabel>
+              <Box xs={{ gap: "25px" }}>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="TES" />
+                <FormControlLabel control={<Checkbox />} label="CSV Import" />
+                <FormControlLabel control={<Checkbox />} label="Manual" />
+              </Box>
+            </Grid>
+          </GridRow>
+          <GridRow container spacing={4}>
+            <Grid item md={3}>
+              <CustomPhoneInput defaultCountry="us" label="Sailing Manager Contact" isRequired={true} value={additionalPhone} onChange={(value) => handleAdditionalPhoneChange(value)} />
+            </Grid>
+          </GridRow>
+          <GridRow container spacing={4}>
+            {phones.map((phone, index) => (
+              <Grid item md={3}>
+                <CustomPhoneInput key={index} defaultCountry="us" isRequired={true} label={labels[index]} value={phone} onChange={(value) => handlePhoneChange(value, index)} />
+              </Grid>
+            ))}
+            <Grid item md={3}>
+              <FormLabel htmlFor="clubName" isRequired={true}>
+                Email Address
+              </FormLabel>
+              <StyledInputBase id="clubName" placeholder="e.g abc@email.com" />
+            </Grid>
+          </GridRow>
+          <Divider />
+          <FormSectionTitle>Other Contacts</FormSectionTitle>
+          <CustomTable headers={headers} data={data} />
         </PageBody>
       </Box>
     </Box>
---
+++ src/ui/FormSectionTitle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const SectionTitle = styled.div`
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 1.8px;
+    text-transform: uppercase;
+    color: var(--primary-color);
+    padding: 32px 0;
+`
+
+export default function FormSectionTitle({ children }) {
+    return (
+        <SectionTitle>{children}</SectionTitle>
+    )
+}
