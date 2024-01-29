2de1bb061b7fecf033600899538d825a53285344
Waleed Waheed <waleed.waheed@globalnorthstar.com>
2024-01-30 01:07:15 +0500

    Waleed

--- src/ui/FormLabel.js
+++
@@ -1,7 +0,0 @@
-import { FormLabel, Typography } from '@mui/material';
-
-export default function FormLabelComponent({ children, isRequired, htmlFor }) {
-  return (
-    <FormLabel htmlFor={htmlFor}>{children}{isRequired && <Typography className='required' component="span">*</Typography>}</FormLabel>
-  )
-}
